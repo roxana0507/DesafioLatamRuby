b80440f5e7291957ee17d1cf073e4ced38e4cba1
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-23 17:56:04 -0600

    Using different patterns
    Using different patterns

--- patterns.rb
+++ patterns.rb
@@ -7,3 +7,16 @@ n.times do |i|
     print "*" #usamos print para que no haga el salto de linea
 end
 #para verlo en la terminal ponemos el nombre del archivo y el numero de veces
+
+#PUNTOS Y NUMEROS
+#1. Identificar el patron, en este caso es que los numeros impares se reemplazan por puntos
+x = ARGV[0].to_i
+
+x.times do |i|
+    if i.even?
+    print i
+    else
+        print "."
+    end
+end
+print "\n"
