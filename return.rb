f1b71ab706ccfd51f7f54c2060f33aad2591e946
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-24 15:00:43 -0600

    return
    return

--- return.rb
+++ return.rb
@@ -1,5 +1,9 @@
-#en ruby los retornos son implicitos
+#en ruby los retornos son implicitos--los retornoa ayudan al metodo a ser mas flexible
 def transformar_a_fahrenheit(f)
     celsius = (f + 40) / 1.8 - 40
     return celsius
+    celsius = 100 #todo lo que viene despues de la instrucccion return es ignorado
 end
+
+puts transformar_a_fahrenheit(123)
+
