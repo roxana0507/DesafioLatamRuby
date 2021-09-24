f91c047fa919557b1e2dbe70e0254cd255eec35c
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-23 22:55:44 -0600

    Method definition
    Method definition

---
+++ methods.rb
@@ -0,0 +1,16 @@
+# METODOS-son formas de agrupar instrucciones y reutilizarlas.
+# Se les conoce con los nombres de procedimiento, funciones, subrutina o metodo.
+# DRY -Do not repeat yourself- evitar que el programador repita código.
+# DEFINICION DE UN METODO:cuando creamos un metodo
+# LLAMAR UN METODO: cuando lo usamos o invocamos
+
+# Existen diferentes lugares para definir un metodo y de eso depende como se an llamados: dentro del espacio principal.
+
+# Los metodos se organizan desde clases y módulos.
+#Orden de ejecucion: de arriba hacia abajo- primero se define luego se llaman.
+#Los parentesis son opcionales, si se usan se colocan soin espacio al lado del nombre.
+
+def nombre_metodo
+    #cuerpo
+    puts "Menú"
+end
